e4ff3658061602349a91c304ec9a34324a474d7c
Robson Gomes <contato@robsongomes.com.br>
2020-08-21 11:31:45 -0300

    Buscando nome do usuario git

--- src/Main.vue
+++ src/Main.vue
@@ -12,10 +12,15 @@
 </template>
 
 <script>
+
+const name = require('child_process')
+  .execSync('git config user.name')
+  .toString().trim()
+
 export default {
     data(){
         return {
-            title: 'Olá mundo, meu nome é Robson!',
+            title: `Olá mundo, meu nome é ${name?name:"(informe com o comando git config user.name 'Seu nome')"}!`,
             sub_title: 'Este é apenas um exemplo simples de uma aplicação com webpack e typescript!!!',
             img : require('src/css/images/webpack-ts.jpg').default
         }
